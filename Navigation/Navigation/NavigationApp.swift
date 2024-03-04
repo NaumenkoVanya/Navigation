2103e759e2483630b14ae0ff6c1d6532c659faa7
Vanya <whitewolf3310@icloud.com>
2024-03-05 01:07:22 +0300

    Add new project

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Ваня Науменко on 5.03.24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
